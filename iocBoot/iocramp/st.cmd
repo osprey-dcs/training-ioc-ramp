f2ef16c2ce346d4b01134246c1e2ee927ee3c520
Wayne Lewis <wlewis@ospreydcs.com>
2026-05-21 10:12:29 -0700

    Initial commit of ramp PV IOC

---
+++ iocBoot/iocramp/st.cmd
@@ -0,0 +1,22 @@
+#!../../bin/linux-x86_64/ramp
+
+#- SPDX-FileCopyrightText: 2005 Argonne National Laboratory
+#-
+#- SPDX-License-Identifier: EPICS
+
+#- You may have to change ramp to something else
+#- everywhere it appears in this file
+
+< envPaths
+
+## Register all support components
+dbLoadDatabase "../../dbd/ramp.dbd"
+ramp_registerRecordDeviceDriver(pdbbase) 
+
+## Load record instances
+dbLoadRecords("../../db/ramp.db","P=Test:")
+
+iocInit()
+
+## Start any sequence programs
+#seq sncramp,"user=epics-dev"
